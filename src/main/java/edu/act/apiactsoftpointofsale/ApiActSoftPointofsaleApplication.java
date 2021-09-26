30c25fe9495a3cbde75abd82a1e364ad6776f096
birehaftu <birehaftu@gmail.com>
2021-09-26 13:51:00 +0300

    allow all incoming connections

--- src/main/java/edu/act/apiactsoftpointofsale/ApiActSoftPointofsaleApplication.java
+++ src/main/java/edu/act/apiactsoftpointofsale/ApiActSoftPointofsaleApplication.java
@@ -7,7 +7,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ApiA /*.allowedOrigins("http://localhost:3000"
+public class ApiActSoftPointofsaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiActSoftPointofsaleApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        /*.allowedOrigins("http://localhost:3000"
                                 ,"http://localhost:*"
                                 ,"http://localhost:38262"
                         );*/
