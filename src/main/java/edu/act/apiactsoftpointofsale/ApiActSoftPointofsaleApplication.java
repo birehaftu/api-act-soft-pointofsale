a27e79933fa09509cb3ea380b4ac192c3a3a9abb
birehaftu <birehaftu@gmail.com>
2021-09-26 13:49:39 +0300

    allow all incoming connections

--- src/main/java/edu/act/apiactsoftpointofsale/ApiActSoftPointofsaleApplication.java
+++ src/main/java/edu/act/apiactsoftpointofsale/ApiActSoftPointofsaleApplication.java
@@ -7,22 +7,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ApiActSoftPointofsaleApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ApiActSoftPointofsaleApplication.class, args);
-    }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000"
-                                //,"https://money-transfer-web-app-actse.herokuapp.com",
-                                //"https://biniama.github.io/money-transfer-web-app-actse/"
-                        );
-                //.allowedOrigins("*");
+public class ApiA /*.allowedOrigins("http://localhost:3000"
+                                ,"http://localhost:*"
+                                ,"http://localhost:38262"
+                        );*/
+                .allowedOrigins("*");
             }
         };
     }
